84103eb9ae4ebc2b8d8b43071ee9bcf96a971fb5
Jonathan Laasonen <iphntm@iPHNTMs-MacBook-Pro.local>
2021-12-29 01:02:52 +0100

    fix -refactoring

--- src/services/deleteService.tsx
+++ src/services/deleteService.tsx
@@ -1,97 +1,45 @@
+const fetchData = (endpoint: string, itemId: string) => {
+  fetch(`http://localhost:4000/${endpoint}`, {
+    method: 'DELETE', // or 'PUT'
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ "_id": itemId }),
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
 export const deleteItem = (itemId: string, itemType: ItemType) => {
   switch (itemType) {
     case 'about':
-      fetch('http://localhost:4000/about', {
-        method: 'DELETE', // or 'PUT'
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "_id": itemId }),
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log('Success:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      fetchData('about', itemId);
       break;
 
     case 'skill':
       console.log('delete skill', itemId)
-      fetch('http://localhost:4000/skills', {
-        method: 'DELETE', // or 'PUT'
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "_id": itemId }),
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log('Success:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      fetchData('skills', itemId);
       break;
 
     case 'work':
       console.log('delete work', itemId)
-      fetch('http://localhost:4000/work', {
-        method: 'DELETE', // or 'PUT'
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "_id": itemId }),
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log('Success:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      fetchData('work', itemId);
       break;
 
     case 'studies':
       console.log('delete studies', itemId)
-      fetch('http://localhost:4000/studies', {
-        method: 'DELETE', // or 'PUT'
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "_id": itemId }),
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log('Success:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      fetchData('studies', itemId);
       break;
 
     case 'project':
       console.log('delete project', itemId)
-      fetch('http://localhost:4000/projects', {
-        method: 'DELETE', // or 'PUT'
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ "_id": itemId }),
-      })
-        .then(res => res.json())
-        .then(data => {
-          console.log('Success:', data);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      fetchData('projects', itemId);
       break;
 
     default:
