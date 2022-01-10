c31a35348a4641ab45de7d96c95d39b3b8ca8194
Jonathan Laasonen <iphntm@iPHNTMs-MacBook-Pro.local>
2022-01-10 22:02:55 +0100

    deployable

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
-export const API_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'https://jonalaa-portfolio-server.herokuapp.com'
-    : 'https://jonalaa-portfolio-server.herokuapp.com';
+// export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://jonalaa-portfolio-server.herokuapp.com';
